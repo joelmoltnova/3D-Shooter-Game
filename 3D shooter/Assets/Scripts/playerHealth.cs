68dc94eb779533b06c47b3af11c7e5d6d86d450a
dennis5132 <dennisvkrevel@gmail.com>
2025-10-06 13:48:04 +0200

    animation tests

--- 3D shooter/Assets/Scripts/playerHealth.cs
+++ 3D shooter/Assets/Scripts/playerHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -18,6 +19,7 @@ public class PlayerHealth : MonoBehaviour
         if (health <= 0)
         {
             Debug.Log("game over");
+            SceneManager.LoadScene("1 - Main Menu");
         }
     }
 }
